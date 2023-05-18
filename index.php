3f81c4227276a5a17456b59baafb810f674bd798
chloescorer <chloe02@outlook.com>
2023-05-18 16:49:34 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php get_header();?>
+<!-- Blog Section -->
+<section class="container-fluid blogsection">
+    <div class="container">
+        <h2 class="blogtag">Explore the Wreckage</h2>
+        <div class="row">
+            <!-- Blog Post 1 -->
+            <div class="col-md-4">
+                <img class="img-fluid blogimages" src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/postthumbnail1.jpg"  alt="Underwater Image of the Batavia">
+                <h3 class="blogheadingcontainer"> AN ARCHAEOLOGICAL EXPLORATION OF THE BATAVIA WRECK </h3>
+                <p class="blogpost">Dr Jane Smith, Batavia Archaeologist,  delves into the exploration of the Batavia shipwreck, including the discovery of artefacts and how they provide insights into life onboard.</p>
+                <p class="dateindex" >Feb 16 2023</p>
+                <a class="readmore" href="blog1.html">Read More...</a>
+            </div> <!-- Column -->
+            
+            <!-- Blog Post 2 -->
+            <div class="col-md-4">
+                <img class="img-fluid blogimages" src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/postthumbnail2.jpg" alt="Survivors Monument Batavia">
+                <h3> A TALE OF RESILIENCE: BATAVIA SHIPWRECK SURVIVORS </h3>
+                <p class="blogpost">Sarah Jones, WA Historian, tells the story of the survivors of the Batavia wreck, including their struggles to survive and the events that led to their eventual rescue.</p>
+                <p class="dateindex">Jan 16 2023</p>
+                <a class="readmore" href="blog2.html">Read More...</a>
+            </div> <!-- Column -->
+            
+            <!-- Blog Post 3 -->
+            <div class="col-md-4">
+                <img class="img-fluid blogimages" src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/postthumbnail3.jpg" alt="Black and White Image of Fremantle">
+                    <h3> A COMPREHENSIVE GUIDE TO SEEING THE BATAVIA TODAY </h3>
+                    <p class="blogpost">John Smith, Travel Writer, provides a  guide to visiting the Batavia, with information about how to get there, what to expect and how to make the most of your visit.</p>
+                    <p class="dateindex">Dec 16 2022</p>
+                    <a class="readmore" href="blog3.html">Read More...</a>
+                </div> <!-- Column -->
+            </div> <!-- Row -->
+        </div> <!-- Container -->
+    </section> <!-- Container Fluid -->
+
+<!-- Tour Section -->
+<section class="container-fluid toursection">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="negmargin">
+                <h4 class="tourheading">BOOK A TOUR</h4>
+                <p class="tourpara">The WA Shipwreck Museum presents the Batavia Gallery. The Batavia Shipwreck is one of the most haunting and tragic pieces of maritime history that is not to be missed. The tour will take you on a journey through time as you explore the gruesome events that unfolded in 1629. Book your tour today and discover the secrets of the WA History for yourself.</p>
+                <button type="button" class="btn tourbtn" onclick="window.location.href = 'contact.html';">BUY TICKETS</button>
+            </div> <!-- Negative Margin Container -->
+            </div> <!-- Column -->
+            <div class="col-sm-6">
+                <!-- <img class="img-fluid skeleton" src="images/skeleton.JPG" alt="Human Skeleton Batavia Display"> -->
+            </div> <!-- Column -->
+        </div> <!-- Row -->
+    </div> <!-- Container -->
+</section> <!-- Container Fluid -->
+<?php get_footer();?>
