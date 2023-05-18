66ce6600b10987ca883a4ce816bfceb1676f9fa4
chloescorer <chloe02@outlook.com>
2023-05-18 16:49:42 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,34 @@
+<!-- Footer -->
+<footer class="container-fluid">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-4">
+                <a href="#">Back to Top</a>
+                <div class="icons">
+                    <a href="https://m.facebook.com/profile.php?id=192629781272213" target="_blank"><img src="images/facebook.png" alt="facebook icon"></a> 
+                    <a href="https://www.instagram.com/wamuseum/?hl=en" target="_blank"><img src="images/instagram.png" alt="instagram icon"></a> 
+                    <a href="https://twitter.com/wamuseum?lang=en" target="_blank"><img src="images/twitter.png" alt="twitter icon"></a> 
+                </div> <!-- Icons -->
+            </div><!-- Column -->
+            <div class="col-md-4">
+                <img src="images/logo.png" class="footerlogo" alt="logo">
+            </div><!-- Column -->
+            <div class="col-md-4">
+                <ul class="footerlinks">
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="blog.html">Articles</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contact</a>
+                    </li>
+                </ul><!-- Footer Links -->
+            </div><!-- Column -->
+        </div><!-- Row -->
+    </div><!-- Container -->
+</footer><!-- Container Fluid -->
+<?php wp_footer(); ?>
+</body>
+</html>
