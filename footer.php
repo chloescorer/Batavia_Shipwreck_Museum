2ad5ef94f888d21c9161b6667a371947d191f40a
chloescorer <chloe02@outlook.com>
2023-05-24 11:33:19 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,13 +5,13 @@
             <div class="col-md-4">
                 <a href="#">Back to Top</a>
                 <div class="icons">
-                    <a href="https://m.facebook.com/profile.php?id=192629781272213" target="_blank"><img src="images/facebook.png" alt="facebook icon"></a> 
-                    <a href="https://www.instagram.com/wamuseum/?hl=en" target="_blank"><img src="images/instagram.png" alt="instagram icon"></a> 
-                    <a href="https://twitter.com/wamuseum?lang=en" target="_blank"><img src="images/twitter.png" alt="twitter icon"></a> 
+                    <a href="https://m.facebook.com/profile.php?id=192629781272213" target="_blank"><img src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/facebook.png" alt="facebook icon"></a> 
+                    <a href="https://www.instagram.com/wamuseum/?hl=en" target="_blank"><img src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/instagram.png" alt="instagram icon"></a> 
+                    <a href="https://twitter.com/wamuseum?lang=en" target="_blank"><img src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/twitter.png" alt="twitter icon"></a> 
                 </div> <!-- Icons -->
             </div><!-- Column -->
             <div class="col-md-4">
-                <img src="images/logo.png" class="footerlogo" alt="logo">
+                <img src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/logo.png" class="footerlogo" alt="logo">
             </div><!-- Column -->
             <div class="col-md-4">
                 <ul class="footerlinks">
