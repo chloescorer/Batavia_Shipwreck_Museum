a26e90ba863ed6063e1fe1f5e9831934067a2152
chloescorer <chloe02@outlook.com>
2023-05-25 12:22:27 +0800

    Create articles_header.php
    header for each individual post

---
+++ articles_header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="<?php bloginfo('charset'); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!-- <meta name="The Shocking History of the Batavia Shipwreck" content="The Batavia Gallery at the WA Shipwrecks Museum">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Quattrocento:wght@700&display=swap" rel="stylesheet">
+<!-- <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="responsive.css"> -->
+<title><?php
+              if (function_exists('is_tag') && is_tag()) {
+                 single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+              elseif (is_archive()) {
+                 wp_title(''); echo ' Archive - '; }
+              elseif (is_search()) {
+                 echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+              elseif (!(is_404()) && (is_single()) || (is_page())) {
+                 wp_title(''); echo ' - '; }
+              elseif (is_404()) {
+                 echo 'Not Found - '; }
+              if (is_home()) {
+                 bloginfo('name'); echo ' - '; bloginfo('description'); }
+              else {
+                  bloginfo('name'); }
+              if ($paged>1) {
+                 echo ' - page '. $paged; }
+           ?>
+</title>
+<meta name="description" content="<?php bloginfo('description'); ?>">
+<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico">
+</head>
+
+<body>
+    <!-- Header -->
+    <header class="container-fluid articleheader">
+        <div class="container">
+            <!-- Navigation Bar -->
+            <nav class="navbar navbar-expand-md navbar-dark">
+                <!-- Brand -->
+                <a class="navbar-brand" href="index.html">
+                    <img src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/logo.png" class="logo" alt="Brand Logo">
+                </a>
+                <!-- Toggler/collapsibe Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+        <?php 
+
+wp_nav_menu( array(
+  'theme_location'  => 'primary',
+  'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+  'container'       => 'div',
+  'container_class' => 'collapse navbar-collapse',
+  'container_id'    => 'bs-example-navbar-collapse-1',
+  'menu_class'      => 'navbar-nav ml-auto',
+  'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+  'walker'          => new WP_Bootstrap_Navwalker(),
+) );
+
+?>
+        </nav>
+        <div class="tag">
+            <h1 class="articleheading text-right">A DEEP DIVE: <span class="redtext">ARTICLES</span></h1>
+        </div> <!-- Tagline Div -->
+    </div> <!-- Container -->
+    <?php wp_head(); ?>
+</header> <!-- Container Fluid -->
