962a18ac7d2686c206c55f3bcb388afac9c5e758
chloescorer <chloe02@outlook.com>
2023-06-07 18:10:48 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -24,3 +24,45 @@ register_nav_menus( array(
 <?php
 add_theme_support('post-thumbnails'); 
 ?>
+<?php
+function footer_left() {
+
+  register_sidebar( array(
+    'name'          => 'footer-left',
+    'id'            => 'footer-l',
+    'before_widget' => '<div>',
+    'after_widget'  => '</div>',
+  
+  ) );
+
+}
+add_action( 'widgets_init', 'footer_left' );
+?>
+<?php
+function footer_middle() {
+
+  register_sidebar( array(
+    'name'          => 'footer-middle',
+    'id'            => 'footer-m',
+    'before_widget' => '<div>',
+    'after_widget'  => '</div>',
+  
+  ) );
+
+}
+add_action( 'widgets_init', 'footer_middle' );
+?>
+<?php
+function footer_right() {
+
+  register_sidebar( array(
+    'name'          => 'footer-right',
+    'id'            => 'footer-r',
+    'before_widget' => '<div>',
+    'after_widget'  => '</div>',
+  
+  ) );
+
+}
+add_action( 'widgets_init', 'footer_right' );
+?>
