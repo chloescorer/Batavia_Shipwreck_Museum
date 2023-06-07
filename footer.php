802ad98245cc34d5ebab70fe6649206a5ceab5f7
chloescorer <chloe02@outlook.com>
2023-06-07 18:10:49 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,18 +3,25 @@
     <div class="container text-center">
         <div class="row">
             <div class="col-md-4">
-                <a href="#">Back to Top</a>
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+            endif; ?>
+
+                <!-- <a href="#">Back to Top</a>
                 <div class="icons">
                     <a href="https://m.facebook.com/profile.php?id=192629781272213" target="_blank"><img src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/facebook.png" alt="facebook icon"></a> 
                     <a href="https://www.instagram.com/wamuseum/?hl=en" target="_blank"><img src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/instagram.png" alt="instagram icon"></a> 
-                    <a href="https://twitter.com/wamuseum?lang=en" target="_blank"><img src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/twitter.png" alt="twitter icon"></a> 
+                    <a href="https://twitter.com/wamuseum?lang=en" target="_blank"><img src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/twitter.png" alt="twitter icon"></a>  -->
                 </div> <!-- Icons -->
             </div><!-- Column -->
             <div class="col-md-4">
-                <img src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/logo.png" class="footerlogo" alt="logo">
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-middle') ) : 
+            endif; ?>
+                <!-- <img src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/logo.png" class="footerlogo" alt="logo"> -->
             </div><!-- Column -->
             <div class="col-md-4">
-                <ul class="footerlinks">
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
+            endif; ?>
+               <!--  <ul class="footerlinks">
                     <li>
                         <a href="index.html">Home</a>
                     </li>
@@ -24,7 +31,7 @@
                     <li>
                         <a href="contact.html">Contact</a>
                     </li>
-                </ul><!-- Footer Links -->
+                </ul> Footer Links -->
             </div><!-- Column -->
         </div><!-- Row -->
     </div><!-- Container -->
