40c5aa6fb7e56b1be92d0d5f200ded0477cb5479
chloescorer <chloe02@outlook.com>
2023-06-07 18:10:45 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -15,16 +15,16 @@ Template Name: Home Page
                 <h3> <?php the_field('indexblogtitle1'); ?> </h3>
                 <p class="blogpost"> <?php the_field('indexblogexcerpt1'); ?> </p>
                 <p class="dateindex"> <?php the_field('indexblogdate1'); ?></p>
-                <a class="readmore" <?php the_field('indexreadmore1'); ?>>Read More...</a>
+                <a class="readmore" href="<?php the_field('indexreadmore1'); ?>">Read More...</a>
             </div> <!-- Column -->
             
             <!-- Blog Post 2 -->
             <div class="col-md-4">
             <img class="img-fluid blogimages" src="<?php the_field('indexblogimg2'); ?>"  alt="Survivors Monument Batavia">
                 <h3> <?php the_field('indexblogtitle2'); ?> </h3>
-                <p class="blogpost"> <?php the_field('indexblogexcerpt2'); ?> </p>
-                <p class="dateindex" ><?php the_field('indexblogdate2'); ?></p>
-                <a class="readmore" <?php the_field('indexreadmore2'); ?>>Read More...</a>
+                <p class="blogpost"><?php the_field('indexblogexcerpt2'); ?> </p>
+                <p class="dateindex"><?php the_field('indexblogdate2'); ?></p>
+                <a class="readmore" href="<?php the_field('indexreadmore2'); ?>">Read More...</a>
             </div> <!-- Column -->
             
             <!-- Blog Post 3 -->
@@ -33,7 +33,7 @@ Template Name: Home Page
                 <h3> <?php the_field('indexblogtitle3'); ?> </h3>
                 <p class="blogpost"> <?php the_field('indexblogexcerpt3'); ?> </p>
                 <p class="dateindex" ><?php the_field('indexblogdate3'); ?></p>
-                <a class="readmore" <?php the_field('indexreadmore3'); ?>>Read More...</a>
+                <a class="readmore" href="<?php the_field('indexreadmore3'); ?>">Read More...</a>
                 </div> <!-- Column -->
             </div> <!-- Row -->
         </div> <!-- Container -->
@@ -47,7 +47,8 @@ Template Name: Home Page
                 <div class="negmargin">
                 <h4 class="tourheading"><?php the_field('indextourheading'); ?></h4>
                 <p class="tourpara"><?php the_field('indextourparagraph'); ?></p>
-                <button type="button" class="btn tourbtn" onclick="window.location.href = 'contact.html';">BUY TICKETS</button>
+                
+                <button type="button" class="btn tourbtn" onclick="window.location.href = 'http://170.187.231.66/~mesh18/contact/';">BUY TICKETS</button>
             </div> <!-- Negative Margin Container -->
             </div> <!-- Column -->
             <div class="col-sm-6">
