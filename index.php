e9ac1a80fbf45cc27483f7235c9ee04a486f95d9
chloescorer <chloe02@outlook.com>
2023-06-07 18:09:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,52 +1,56 @@
+<?php
+/*
+Template Name: Home Page
+*/
+?>
 <?php get_header();?>
 <!-- Blog Section -->
 <section class="container-fluid blogsection">
     <div class="container">
-        <h2 class="blogtag">Explore the Wreckage</h2>
+    <h2 class="blogtag"><?php the_field('indexblogtag'); ?></h2><!--  Custom Field for Blog Tagline-->
         <div class="row">
             <!-- Blog Post 1 -->
             <div class="col-md-4">
-                <img class="img-fluid blogimages" src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/post1thumbnail.jpg"  alt="Underwater Image of the Batavia">
-                <h3 class="blogheadingcontainer"> AN ARCHAEOLOGICAL EXPLORATION OF THE BATAVIA WRECK </h3>
-                <p class="blogpost">Dr Jane Smith, Batavia Archaeologist,  delves into the exploration of the Batavia shipwreck, including the discovery of artefacts and how they provide insights into life onboard.</p>
-                <p class="dateindex" >Feb 16 2023</p>
-                <a class="readmore" href="blog1.html">Read More...</a>
+                <img class="img-fluid blogimages" src="<?php the_field('indexblogimg1'); ?>"  alt="Underwater Image of the Batavia">
+                <h3> <?php the_field('indexblogtitle1'); ?> </h3>
+                <p class="blogpost"> <?php the_field('indexblogexcerpt1'); ?> </p>
+                <p class="dateindex"> <?php the_field('indexblogdate1'); ?></p>
+                <a class="readmore" href="<?php the_field('indexreadmore1'); ?>">Read More...</a>
             </div> <!-- Column -->
             
             <!-- Blog Post 2 -->
             <div class="col-md-4">
-                <img class="img-fluid blogimages" src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/post2thumbnail.jpg" alt="Survivors Monument Batavia">
-                <h3> A TALE OF RESILIENCE: BATAVIA SHIPWRECK SURVIVORS </h3>
-                <p class="blogpost">Sarah Jones, WA Historian, tells the story of the survivors of the Batavia wreck, including their struggles to survive and the events that led to their eventual rescue.</p>
-                <p class="dateindex">Jan 16 2023</p>
-                <a class="readmore" href="blog2.html">Read More...</a>
+            <img class="img-fluid blogimages" src="<?php the_field('indexblogimg2'); ?>"  alt="Survivors Monument Batavia">
+                <h3> <?php the_field('indexblogtitle2'); ?> </h3>
+                <p class="blogpost"><?php the_field('indexblogexcerpt2'); ?> </p>
+                <p class="dateindex"><?php the_field('indexblogdate2'); ?></p>
+                <a class="readmore" href="<?php the_field('indexreadmore2'); ?>">Read More...</a>
             </div> <!-- Column -->
             
             <!-- Blog Post 3 -->
             <div class="col-md-4">
-                <img class="img-fluid blogimages" src="http://170.187.231.66/~mesh18/wp-content/themes/batavia_shipwreck_museum/images/post3thumbnail.jpg" alt="Black and White Image of Fremantle">
-                    <h3> A COMPREHENSIVE GUIDE TO SEEING THE BATAVIA TODAY </h3>
-                    <p class="blogpost">John Smith, Travel Writer, provides a  guide to visiting the Batavia, with information about how to get there, what to expect and how to make the most of your visit.</p>
-                    <p class="dateindex">Dec 16 2022</p>
-                    <a class="readmore" href="blog3.html">Read More...</a>
+            <img class="img-fluid blogimages" src="<?php the_field('indexblogimg3'); ?>"  alt="Black and White Image of Fremantle">
+                <h3> <?php the_field('indexblogtitle3'); ?> </h3>
+                <p class="blogpost"> <?php the_field('indexblogexcerpt3'); ?> </p>
+                <p class="dateindex" ><?php the_field('indexblogdate3'); ?></p>
+                <a class="readmore" href="<?php the_field('indexreadmore3'); ?>">Read More...</a>
                 </div> <!-- Column -->
             </div> <!-- Row -->
         </div> <!-- Container -->
     </section> <!-- Container Fluid -->
 
 <!-- Tour Section -->
-<section class="container-fluid toursection">
+<section class="container-fluid toursection" style="background-size: cover; background-image: url(<?php the_field('tourbackgroundimg') ?>);">
     <div class="container">
         <div class="row">
             <div class="col-sm-6">
                 <div class="negmargin">
-                <h4 class="tourheading">BOOK A TOUR</h4>
-                <p class="tourpara">The WA Shipwreck Museum presents the Batavia Gallery. The Batavia Shipwreck is one of the most haunting and tragic pieces of maritime history that is not to be missed. The tour will take you on a journey through time as you explore the gruesome events that unfolded in 1629. Book your tour today and discover the secrets of the WA History for yourself.</p>
-                <button type="button" class="btn tourbtn" onclick="window.location.href = 'contact.html';">BUY TICKETS</button>
+                <h4 class="tourheading"><?php the_field('indextourheading'); ?></h4>
+                <p class="tourpara"><?php the_field('indextourparagraph'); ?></p>
+                <button type="button" class="btn tourbtn" onclick="window.location.href = '<?php the_field('ticketbutton') ?>';">BUY TICKETS</button>
             </div> <!-- Negative Margin Container -->
             </div> <!-- Column -->
             <div class="col-sm-6">
-                <!-- <img class="img-fluid skeleton" src="images/skeleton.JPG" alt="Human Skeleton Batavia Display"> -->
             </div> <!-- Column -->
         </div> <!-- Row -->
     </div> <!-- Container -->
